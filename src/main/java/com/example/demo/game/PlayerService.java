8c4596a57f821ae34c34b738a2eb41fea7db5709
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-05-04 15:15:34 +0300

    final

--- src/main/java/com/example/demo/game/PlayerService.java
+++ src/main/java/com/example/demo/game/PlayerService.java
@@ -13,18 +13,23 @@ public class PlayerService {
     @Autowired
     PlayerRepository repository;
 
+    /**
+     * adauga in baza de date un player
+     * @param student
+     */
     public void save(final Player student) {
         repository.save(student);
     }
 
+    /**
+     * returneaza toti playerii din baza de date
+     * @return
+     */
     public List<Player> getAll() {
         final List<Player> players = new ArrayList<>();
         repository.findAll().forEach(players::add);
         return players;
     }
 
-    public void update(int id){
-
-    }
 
 }
